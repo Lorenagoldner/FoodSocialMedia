2a793db41eba7a311f0f8ce072785b2a2c42b93d
Cássia Damas <cassiadamassilva@gmail.com>
2026-05-17 22:31:14 +0100

    Implement BCrypt hash de passwords

--- BlazorAppFood/Data/LoginService.cs
+++ BlazorAppFood/Data/LoginService.cs
@@ -44,7 +44,7 @@ namespace BlazorAppFood.Data
                 if (isValid)
                 {
                     // Store email in sessionStorage after successful login
-                    await _sessionStorage.SetItemAsync("email", Email);
+                    await _sessionStorage.SetItemAsync("userEmail", Email);
                     return true;
                 }
                 else
