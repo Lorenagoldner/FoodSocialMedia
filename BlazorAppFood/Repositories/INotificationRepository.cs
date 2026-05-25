ecd8146c5f1f0a81f2deb3bd8b7aced09ab94a36
Lorena <lologoldner@gmail.com>
2026-05-25 13:03:58 +0100

    Correção de arquivos por causa do merge.Alterado onde estava Service pra Repository. Correção da NavMenu.

---
+++ BlazorAppFood/Repositories/INotificationRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BlazorAppFood.Models;
+
+namespace BlazorAppFood.Data
+{
+    public interface INotificationRepository
+    {
+        public interface INotificationRepository
+        {
+            Task CreateNotification(Notification notification);
+
+            Task<List<Notification>> GetUserNotifications(int userId);
+
+            Task MarkAsRead(int notificationId);
+        }
+    }
+}
